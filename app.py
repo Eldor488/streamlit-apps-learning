a7dc7dabdc9c60376678133ce424283d791d70a7
Enmanuel De los Santos Cruz <116897148+Eldor488@users.noreply.github.com>
2024-03-30 08:38:22 -0600

    creating first app

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# This line will add a simple message to the app 
+
+st.write("Hello, Streamlit! I'm so excited to be here"); 
+st.write("I'm gonna learn so much doing this.");
